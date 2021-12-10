4aa40e0298c4e7542678581680a124a7b39fb7a8
Gabriel Lamas <gabrielldarts@gmail.com>
2021-12-10 18:40:21 -0300

    training real exercises

--- api/Trainings.js
+++ api/Trainings.js
@@ -1,85 +1,131 @@
 const Trainings = [
   {
     type: "a",
+    custom_name: "Pernetas",
     exercises: [
       {
-        name: "Agachamento",
-        repetition: "3x6-8",
+        name: "Leg press",
+        repetition: "4x10-20",
+        help_image:
+          "https://www.dicasdetreino.com.br/wp-content/uploads/2017/05/posi%C3%A7%C3%A3o-dos-p%C3%A9s-no-Leg-Pres.jpg",
       },
       {
         name: "Agachamento",
         repetition: "3x6-8",
+        help_image:
+          "http://conteudo.imguol.com.br/c/entretenimento/d0/2018/10/18/mulher-fazendo-agachamento-livre-com-barra-1539887183808_v2_1077x787.jpg",
       },
       {
-        name: "Agachamento",
+        name: "Mesa extensora",
         repetition: "3x6-8",
+        help_image: "https://thumb.mais.uol.com.br/16772110.jpg",
+      },
+      {
+        name: "Mesa flexora",
+        repetition: "3x6-8",
+        help_image:
+          "http://conteudo.imguol.com.br/c/entretenimento/a4/2019/11/18/mesa-flexora-1574088009594_v2_900x506.jpg",
+      },
+      {
+        name: "Panturrilhas",
+        repetition: "3x6-8",
+        help_image: "https://thumb.mais.uol.com.br/16772110.jpg",
       },
     ],
   },
   {
     type: "b",
+    custom_name: "Peito e Ombro",
     exercises: [
       {
-        name: "Bchamento",
+        name: "Supino reto",
         repetition: "3x6-8",
       },
       {
-        name: "Bchamento",
+        name: "Supino inclinado alter",
         repetition: "3x6-8",
       },
       {
-        name: "Bchamento",
+        name: "Crossover",
+        repetition: "3x6-8",
+      },
+      {
+        name: "Voador máquina",
+        repetition: "3x6-8",
+      },
+      {
+        name: "desenvolvimento frontal",
+        repetition: "3x6-8",
+      },
+      {
+        name: "desenvolvimento lateral",
+        repetition: "3x6-8",
+      },
+      {
+        name: "Elevação frontal máquina",
         repetition: "3x6-8",
       },
     ],
   },
   {
     type: "c",
+    custom_name: "Costas & Abs",
     exercises: [
       {
-        name: "Cchamento",
+        name: "Remada unilateral com halter (serrote)",
         repetition: "3x6-8",
       },
       {
-        name: "Cchamento",
+        name: "Puxada frente pegada aberta (pulley)",
+        repetition: "3x6-8",
+        help_image:
+          "https://treinomestre.com.br/wp-content/uploads/2018/10/pulley-frente-puxador-.jpg",
+      },
+      {
+        name: "Puxada frontal com triângulo (pulley)",
         repetition: "3x6-8",
       },
       {
-        name: "Cchamento",
+        name: "Hiperextensão lombar com peso",
         repetition: "3x6-8",
+        help_image:
+          "https://www.mundoboaforma.com.br/wp-content/uploads/2021/08/exercicio-hiperextensao-lombar400.jpg",
+      },
+      {
+        name: "Remada máquina ",
+        repetition: "3x6-8",
+        help_image: "https://i.ytimg.com/vi/t4jOtaBPR10/maxresdefault.jpg",
       },
     ],
   },
   {
     type: "d",
+    custom_name: "bíceps, tríceps e antebraço",
     exercises: [
       {
-        name: "Dchamento",
+        name: "Rosca direta com barra",
         repetition: "3x6-8",
       },
       {
-        name: "Dchamento",
+        name: "Rosca alternada com halteres",
         repetition: "3x6-8",
       },
       {
-        name: "Dchamento",
+        name: "Rosca no banco Scott",
         repetition: "3x6-8",
+        help_image:
+          "https://blog.lionfitness.com.br/wp-content/uploads/2018/11/rosca-scott-equipamentos-lion-fitness.jpg",
       },
-    ],
-  },
-  {
-    type: "e",
-    exercises: [
       {
-        name: "Echamento",
+        name: "Tríceps no pulley",
         repetition: "3x6-8",
       },
       {
-        name: "Echamento",
+        name: "Paralela peso corpo",
         repetition: "3x6-8",
       },
       {
-        name: "Echamento",
+        name: "Triceps no banco fritar",
         repetition: "3x6-8",
       },
     ],
